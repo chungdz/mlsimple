5c9cd28f63740d2e2ee0271abce427b4df844645
Microsoft <1033718596@qq.com>
2022-07-20 10:38:01 -0700

    add file path

--- preprocess/get_meta.py
+++ preprocess/get_meta.py
@@ -10,12 +10,16 @@ parser.add_argument("--dpath", default="data", type=str,
                         help="root path of all data")
 parser.add_argument("--filep", default="sample.tsv", type=str,
                         help="train file")
+parser.add_argument("--headp", default="header.tsv", type=str,
+                        help="train file")
 args = parser.parse_args()
 
 filep = os.path.join(args.dpath, args.filep)
+headp = os.path.join(args.dpath, args.headp)
 outp = os.path.join(args.dpath, "meta_info.json")
-
-df = pd.read_csv(filep, sep='\t')
+print('load data')
+header = pd.read_csv(headp, sep='\t')
+df = pd.read_csv(filep, sep='\t', names=header.columns)
 
 flist = [x for x in list(df.columns) if 'Feature' in x]
 min_list = []
--- run.sh
+++ run.sh
@@ -2,5 +2,10 @@ source ~/.bashrc
 
 mkdir data cps
 python -m preprocess.get_meta
+
 python train.py
 python train.py --with_id=0
+
+python -m preprocess.get_meta --dpath=/work/yunfanhu/mannual
+python train.py --dpath=/work/yunfanhu/mannual
+

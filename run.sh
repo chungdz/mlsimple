b03a3d06112829d31f9e2647ee0934d83a4ad599
Microsoft <1033718596@qq.com>
2022-07-21 16:06:18 -0700

    generate scopes

--- run.sh
+++ run.sh
@@ -10,6 +10,7 @@ python train_catboost.py
 python predict.py --load_from=cps/checkpoint-250/pytorch_model.bin
 
 python -m preprocess.get_meta --dpath=/data/yunfanhu/mannual --filep=sample_1M.tsv
-python train.py --dpath=/data/yunfanhu/mannual --filep=sample_1M.tsv
+python train.py --dpath=/data/yunfanhu/mannual --filep=sample_1M.tsv --epoch=3
+python train.py --dpath=/data/yunfanhu/mannual --filep=sample_1M.tsv --with_id=0  --epoch=3
 
 python train_catboost.py  --dpath=/data/yunfanhu/mannual --filep=sample_1M.tsv

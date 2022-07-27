5bf78969aa1ac063641067b68316d1d60687289b
Microsoft <1033718596@qq.com>
2022-07-27 10:02:29 -0700

    bigger

--- nn_modules/noid.py
+++ nn_modules/noid.py
@@ -17,9 +17,9 @@ class NoID(nn.Module):
             nn.Sigmoid()
         )
         self.seq2 = nn.Sequential(
-            nn.Linear(self.flen, 1000),
+            nn.Linear(self.flen, 10000),
             nn.ReLU(),
-            nn.Linear(1000, 1000),
+            nn.Linear(10000, 1000),
             nn.ReLU(),
             nn.Linear(1000, 1000),
             nn.Sigmoid()

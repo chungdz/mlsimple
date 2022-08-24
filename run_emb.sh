abfb0471265b1113c16b087849a2e06d283e8b9d
Microsoft <1033718596@qq.com>
2022-08-24 10:55:12 -0700

    run sh update

--- run_emb.sh
+++ run_emb.sh
@@ -1,5 +1,10 @@
 # 14 train and 7 valid
-python -m preprocess.get_meta_emb --dpath=/data/yunfanhu/samples_emb --filep=train.tsv --vfilep=valid.tsv --chunk_size=50000
+python -m preprocess.get_meta_emb --dpath=/data/yunfanhu/samples_emb \
+                                --filep=train.tsv \
+                                --vfilep=valid.tsv \
+                                --chunk_size=50000
+python -m preprocess.process_meta --dpath=/data/yunfanhu/samples_emb \
+                                    --drop_num=10
 # origin
 python train_emb.py --dpath=/data/yunfanhu/samples_emb \
                     --batch_size=2 \
